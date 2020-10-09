eb941748ea8eb96cfe349a04b9553395019b13f0
Chris C <christopher.cruzcosa@gmail.com>
2020-10-09 00:34:00 -0700

    added main functionality and path connections

--- server.js
+++ server.js
@@ -8,55 +8,87 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+let noteCount = 0;
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+app.use(express.static(path.join(__dirname, '/public')));
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, () => {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
 
 // Routes
 // =============================================================
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
-// let notesJson = fs.readFile("./db/db.json", "utf8", (err, data) => {
-//   console.log(data);
-//   return data
-// })
 
 //Displays all notes
 app.get("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    console.log(data);
-    return res.json(data)
-  });
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+        return res.json(JSON.parse(data))
+    });
 })
 
 app.post("/api/notes", (req, res) => {
-  fs.readFile("./db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
 
-    let arrayOfObjects = JSON.parse(data)
-    arrayOfObjects.push(req.body);
+        let arrayOfObjects = JSON.parse(data);
+        let newNote = req.body
 
-    fs.writeFile("./db/db.json", JSON.stringify(arrayOfObjects), "utf8", (err) => {
-      if (err) throw err;
-      res.json(req.body)
-      console.log('New db file')
-    })
-  });
+        noteCount += 1;
+
+        newNote.id = noteCount;
+
+        arrayOfObjects.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(arrayOfObjects), "utf8", (err) => {
+            if (err) throw err;
+            console.log('New db file')
+            return res.json(arrayOfObjects)
+        })
+    });
 });
+
+
+app.delete("/api/notes/:id", (req, res) => {
+    const chosen = req.params.id;
+    console.log(chosen);
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        let arrayOfObjects = JSON.parse(data);
+        let sliceLocation;
+
+        for (let i = 0; i < arrayOfObjects.length; i++) {
+            if (chosen === arrayOfObjects[i].id) {
+                sliceLocation = i - 1;
+                console.log(sliceLocation);
+            }
+        };
+
+        let newArray = arrayOfObjects.slice(sliceLocation, 1);
+
+        fs.writeFile("./db/db.json", JSON.stringify(newArray), "utf8", (err) => {
+            if (err) throw err;
+            return res.json(arrayOfObjects)
+        })
+    });
+});
+
